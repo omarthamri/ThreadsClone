6ee51b34dcbc6db99d14d58022bb245cf5b68e4f
omarthamri <omarthamri4@gmail.com>
2023-10-23 23:08:47 +0100

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by omar thamri on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
