09e9e28e5f26a2977ba5334fc0297917f4a55bbc
omarthamri <omarthamri4@gmail.com>
2023-10-24 00:09:50 +0100

    implement authentification UI

---
+++ ThreadsClone/Core/Authentification/Views/LoginView.swift
@@ -0,0 +1,78 @@
+//
+//  LoginView.swift
+//  ThreadsClone
+//
+//  Created by omar thamri on 23/10/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                Image("threads_logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .padding()
+                VStack {
+                    TextField("Enter your email",text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding(.horizontal,24)
+                    SecureField("Enter your password",text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding(.horizontal,24)
+                }
+                NavigationLink {
+                    Text("Forgot pwd")
+                } label: {
+                    Text("Forgot password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity,alignment: .trailing)
+                        .padding(.vertical)
+                        .padding(.trailing,28)
+                        .foregroundStyle(.black)
+                }
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 352,height: 44)
+                        .background(.gray)
+                        .foregroundStyle(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .padding(.top)
+                })
+                Spacer()
+                Divider()
+                NavigationLink {
+                    Text("sign up view")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                    .foregroundStyle(.black)
+                }
+                .padding(.vertical,16)
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ ThreadsClone/Core/Authentification/Views/RegitrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegitrationView.swift
+//  ThreadsClone
+//
+//  Created by omar thamri on 24/10/2023.
+//
+
+import SwiftUI
+
+struct RegitrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegitrationView()
+}
